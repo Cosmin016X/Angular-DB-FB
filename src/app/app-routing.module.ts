c3b620bad72ca09880ce4d07356ceef997b581e4
Cosmin Rusu <cosminrusu014@gmail.com>
2022-04-01 21:16:27 +0200

    Actualizacion del codigo v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'formularios', loadChildren: () => import('./pages/employees/formularios/formularios.module').then(m => m.FormulariosModule) }, { path: 'empleados', loadChildren: () => import('./pages/employees/empleados/empleados.module').then(m => m.EmpleadosModule) }, { path: 'information', loadChildren: () => import('./pages/employees/information/information.module').then(m => m.InformationModule) }, { path: 'inicio', loadChildren: () => import('./pages/employees/inicio/inicio.module').then(m => m.InicioModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/employees/empleados/empleados.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-empleados',
+  templateUrl: './empleados.component.html',
+  styleUrls: ['./empleados.component.scss']
+})
+export class EmpleadosComponent implements OnInit {
+
+  navigationExtras: NavigationExtras = {
+    state: {
+      value: null,
+    }
+  };
+  
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  
+  onGoToConfirm(item: any): void {
+    this.navigationExtras.state['value'] = item;
+    this.router.navigate(['/empleados/confirmar'], this.navigationExtras);
+  }
+
+  onGoToEdit(item: any): void {
+    this.navigationExtras.state['value'] = item;
+    this.router.navigate(['/empleados/editar'], this.navigationExtras);
+  }
+
+  onGoToDelete(item: any): void {alert ('Borrado');
+  }
+
+}
+
